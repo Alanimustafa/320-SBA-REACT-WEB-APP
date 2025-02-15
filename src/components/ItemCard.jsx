8a555536307cfbd6ec492c52e960f1e6aeb68df8
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-02-14 21:50:11 -0600

    Items have been displayed in the mainpage - DONE

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -2,6 +2,16 @@
 
 const ItemCard = ({item}) => {
     // console.log("The is the item Card " + item.title) // For Test Only The Item prop
+
+    return (
+        <>
+            <div className="itemCardMainContainer">
+                <h3 className="ItemCardHeader">{item.title}</h3>
+                <img className="ItemCardImage" src={item.image}/>
+                <pre className="ItemCardDescription">{item.description}</pre>
+            </div>
+        </>
+    )
 }
 
 
