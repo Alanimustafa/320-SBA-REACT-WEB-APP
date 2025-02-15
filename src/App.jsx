cc526122d1f6269e02b7e8a507ef5944549d3dae
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-02-14 21:03:40 -0600

    API data has been received successfully from the API source using AXIOS

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,15 @@ import './App.css'
 
 function App() {
   
+  // Create the Items State contant to manage the state using useState with a default empty array.
+  const [items, setItems] = useState([])
+
+  useEffect(()=>{
+    axios.get("https://fakestoreapi.com/products")
+    .then(response => {setItems(response)
+      console.log(response) // Testing the response from the API Source.
+    })
+  },[])
 
   return (
     <>
