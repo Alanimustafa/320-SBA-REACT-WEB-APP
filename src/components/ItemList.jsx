a43f1675c18e8d5f3fb30cbf8f3e66e44cecce3a
Alanimustafa <mustafa.kamel.ani@gmail.com>
2025-02-15 15:57:38 -0600

    ItemList.css has been created to style the ItemList.css to style the items main container

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,8 +1,9 @@
 import ItemCard from "./ItemCard";
+import "./ItemList.css"
 
 const ItemList = ({ items }) => {
     return (
-        <div>
+        <div className="itemListMainContainer">
             {/* Passing the item prop to the ItemCard which display a card for each item in the items array with some information about the product */}
           {items.map(item => <ItemCard key={item.id} item={item} />)}
         </div>
